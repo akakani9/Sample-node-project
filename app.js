79e68b5a438c852fd8c9ce960899ff4cc2e1bfa3
akakani9 <130961972+akakani9@users.noreply.github.com>
2023-08-03 02:57:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
-app.set('views', __dirname + '/app/server/views'); 
-app.set('view engine', 'ejs'); 
-
-require('./app/routes')(app);
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+});
 
 app.listen(port, function(){
 	console.log('Running on Port ' + port + ' .');
